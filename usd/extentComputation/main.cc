bfcf1c7cca5a28b2161bd97ff230afc04b41f1f3
superfunc <superfunc@users.noreply.github.com>
2018-09-01 19:29:25 -0700

    Update script to current version
    - Was off by one, doesn't affect data results, numbers updated running against new code.

--- usd/extentComputation/main.cc
+++ usd/extentComputation/main.cc
@@ -18,11 +18,12 @@ escape(void* p)
 }
 
 #define MK_BENCH(k, n)                                                         \
-  static void BM_ComputeExtent_PT_##k##_GS_##n(benchmark::State& state)        \
+  static void ComputeExtent_NumPoints_##k##_GrainSize_##n(                     \
+    benchmark::State& state)                                                   \
   {                                                                            \
     VtVec3fArray points, extent;                                               \
                                                                                \
-    for (auto i = 0; i < k; ++i) {                                             \
+    for (auto i = 0; i < k+1; ++i) {                                           \
       points.push_back({ float(i), float(i), float(i) });                      \
     }                                                                          \
                                                                                \
@@ -32,15 +33,18 @@ escape(void* p)
       escape(&points);                                                         \
     }                                                                          \
   }                                                                            \
-  BENCHMARK(BM_ComputeExtent_PT_##k##_GS_##n);
+  BENCHMARK(ComputeExtent_NumPoints_##k##_GrainSize_##n);
 
 #define MK_BENCHES(k)                                                          \
   MK_BENCH(k, 1);                                                              \
   MK_BENCH(k, 10);                                                             \
   MK_BENCH(k, 100);                                                            \
+  MK_BENCH(k, 250);                                                            \
+  MK_BENCH(k, 500);                                                            \
   MK_BENCH(k, 1000);                                                           \
   MK_BENCH(k, 10000);                                                          \
-  MK_BENCH(k, 100000);
+  MK_BENCH(k, 100000);                                                         \
+  MK_BENCH(k, 1000000);
 
 MK_BENCHES(1);
 MK_BENCHES(10);
@@ -48,5 +52,6 @@ MK_BENCHES(100);
 MK_BENCHES(1000);
 MK_BENCHES(10000);
 MK_BENCHES(100000);
+MK_BENCHES(1000000);
 
 BENCHMARK_MAIN();
